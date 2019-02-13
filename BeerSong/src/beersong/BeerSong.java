128b1a6010267a20ade9797e15566f3450592cfb
binjushrestha@oakland.edu <binjushrestha@oakland.edu>
2019-02-13 18:36:16 -0500

    implemented

--- BeerSong/src/beersong/BeerSong.java
+++ BeerSong/src/beersong/BeerSong.java
@@ -15,11 +15,28 @@ public class BeerSong {
      * @param args the command line arguments
      */
     
-    static String Ninety_Nine_Bottles_of_Beer(int i) { return i > 1 ? " bottles of beer" : " bottle of beer"; }
-    
-    public static void main(String [] arg) {
-    String [] Ninety_Nine_Bottles_of_Beer = { " on the wall, ", "\nTake one down, pass it round, " };
-    for(int i=99;i>0;i--) System.out.println("" + i + Ninety_Nine_Bottles_of_Beer(i) + Ninety_Nine_Bottles_of_Beer[0] + i + Ninety_Nine_Bottles_of_Beer(i) + Ninety_Nine_Bottles_of_Beer[1] + ((i>1)?i+Ninety_Nine_Bottles_of_Beer(i):"no more bottles of beer"));
-  }
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            if (beerNum == 1) {
+                word = "bottle"; // ONE bottle
+            }
+
+            System.out.println(beerNum + " " + word + " of beer on the wall, " + beerNum + " " + word + " of beer");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println("Take one down, pass it round " + beerNum + " " + word + " of beer");
+            }
+        }
+
+        if (beerNum == 0) {
+            System.out.println("No more bottles of beer");
+        }
+
+    }
 } 
 
